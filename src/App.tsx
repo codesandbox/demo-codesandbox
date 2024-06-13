8d6f2bc15a01e2d96476a52a8e663a487d6f721f
Filipe Lima <45261568+filipelima18@users.noreply.github.com>
2024-06-13 11:15:23 +0000

    chore: update heading copy

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const fetchTemplates = async () => {
     setState({ state: "LOADING" });
     const data = await fetch(
-      "https://codesandbox.io/api/v1/sandboxes/templates/official",
+      "https://codesandbox.io/api/v1/sandboxes/templates/official"
     );
     const json = await data.json();
     const sandboxes = json[0].sandboxes;
@@ -28,7 +28,7 @@ function App() {
       setState({
         state: "READY",
         data: sandboxes.filter((item) =>
-          (item.custom_template?.icon_url ?? "").startsWith("http"),
+          (item.custom_template?.icon_url ?? "").startsWith("http")
         ),
       });
     }, 1000);
@@ -42,7 +42,7 @@ function App() {
     <div className="flex-1 space-y-4 p-4 pt-6 bg-amber-100 min-h-[100vh]">
       <div className="fixed">
         <h1 className="font-thin leading-none text-[70px] tracking-wider mb-4">
-          Discover
+          Find new
           <br /> CodeSandbox
           <br /> templates
         </h1>
